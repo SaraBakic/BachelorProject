1eb5bc9e9571dcaef9f8633e6ab076a9783b5018
IvanUnkovic <113613867+IvanUnkovic@users.noreply.github.com>
2023-01-03 22:48:22 +0100

    upload of scripts analyzing the bedgraph files

---
+++ solutions/Phase3/Part1/neg.py
@@ -0,0 +1,30 @@
+import re
+_RE_COMBINE_WHITESPACE = re.compile(r"\s+")
+
+file_name = "bedGraph_CpG_negative.bedGraph"
+file = open(file_name, "r")
+lines = file.readlines()
+countSum = 0
+countLines = 0
+countNonM = 0
+countM = 0
+for line in lines:
+	if countLines==0:
+		countLines+=1
+	else:
+		line = _RE_COMBINE_WHITESPACE.sub(" ", line).strip()
+		parsed = line.split(' ')
+		if int(parsed[3])==0:
+			countNonM+=1
+		if int(parsed[3])==100:
+			countM+=1
+		countLines+=1
+		countSum+=int(parsed[3])
+file.close()
+
+percentage = countSum/countLines
+
+print("Total number of 0% methylation CpG sites: " + str(countNonM))
+print("Total number of 100% methylation CpG sites: " + str(countM))
+print("Total number of CpG sites: " + str(countLines))
+print("Average percentage of methylation CpG sites: " + str(round(percentage,4)) + "%")
